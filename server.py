c787b312f28851eaaace47941fac5a7be0599e47
regulusv <artemisgray@gmail.com>
2022-02-28 18:57:01 -0800

    lab1

---
+++ server.py
@@ -0,0 +1,41 @@
+# import socket module
+from socket import *
+import sys  # In order to terminate the program
+
+HOST = '127.0.0.1'
+PORT = 6789
+
+
+def webServer(port=6789):
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    # Prepare a sever socket
+    serverSocket.bind((HOST, PORT))
+    serverSocket.listen(1)
+    while True:
+        print("listening")
+        connectionSocket, addr = serverSocket.accept()
+        try:
+            message = connectionSocket.recv(1024)
+            print("msg: ", message)
+            filename = message.split()[1]
+            print("filename: ", message.split()[1])
+            f = open(filename[1:])
+            # Send one HTTP header line into socket
+            outputs = f.read()
+            connectionSocket.send(bytes('HTTP/1.1 200 OK\r\n\r\n', 'UTF-8'))
+            print(outputs)
+            for i in range(0, len(outputs)):
+                connectionSocket.send(outputs[i].encode())
+            connectionSocket.send("\r\n".encode())
+            connectionSocket.close()
+        except IOError:
+            connectionSocket.send(bytes("HTTP/1.1 404 Not Found\r\n\r\n", 'UTF-8'))
+            connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n", 'UTF-8'))
+            # Close the client connection socket
+            connectionSocket.close()
+        serverSocket.close()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    webServer(6789)
